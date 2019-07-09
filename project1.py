e81823297c87a56ec101033b115abca73b3f9c05
SSR2018 <44760408+SSR2018@users.noreply.github.com>
2019-07-09 20:57:01 +0800

    Add files via upload

--- project1.py
+++ project1.py
@@ -20,20 +20,32 @@ class Tushare_data:
     """""
     爬去输出参数的中英文
     """""
-    def spyder(self,id_numc):
+    def spyder(self,id_num):
         url = 'https://tushare.pro/document/2?doc_id='+str(id_num)
         import requests
-        header = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-        t = requests.get(url = url,header = header)
+        headers = headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+        t = requests.get(url = url,headers = headers)
         t.encoding = t.apparent_encoding
         from bs4 import BeautifulSoup
         soup = BeautifulSoup(t.text, 'html.parser')
+
+        if '默认' in str(soup):
+            l = 4
+        else:
+            l = 3
+
         s = soup.find_all('table')[1].find_all('td')
         out_dict = dict()
-        for num in range(len(s)/4):
-            out_dict[s[4*num].text] = s[4*num+3].text
+        for num in range(len(s)//l):
+            out_dict[s[l*num].text] = s[l*num+l-1].text
         return out_dict
-
+    def col2(self,id_num,data):
+        dict = self.spyder(id_num = id_num)
+        list = []
+        for col in data.columns:
+            list.append(dict[col])
+        data.columns = list
+        return data
 
     """""
     shibor
@@ -89,7 +101,7 @@ class Tushare_data:
         #sb_dict = {'ts_code': 'TS代码', 'symbol': '股票代码', 'name': '股票名称', 'area': '所在地域', 'industry': '所属行业', 'fullname': '股票全称'\
         #              , 'enname': '英文名称', 'market': '市场类型', 'curr_type': '交易货币', 'list_status': '上市状态', 'delist_date': '退市日期'\
         #             , 'list_date': '上市日期', 'is_hs': '是否沪深港通标的'}
-        sb_dict = spyder( num  =25)
+        sb_dict = self.spyder( id_num =25)
         sb_list = []
         for col_sb in sb.columns:
             sb_list.append(sb_dict[col_sb])
@@ -139,7 +151,59 @@ class Tushare_data:
             nc_list.append(nc_dict[col_nc])
         nc.columns = nc_list
         return nc
-
+    """""
+    沪深股通成份股
+    接口：hs_const
+    描述：获取沪股通、深股通成分数据
+    名称	类型	必选	描述
+    hs_type	str	Y	类型SH沪股通SZ深股通
+    is_new	str	N	是否最新 1 是 0 否 (默认1)
+    类型	默认显示	描述
+    ts_code	str	Y	TS代码
+    hs_type	str	Y	沪深港通类型SH沪SZ深
+    in_date	str	Y	纳入日期
+    out_date	str	Y	剔除日期
+    is_new	str	Y	是否最新 1是 0否
+    """""
+    def hs_const(self,hs_type = 'SH',is_new = ''):
+        hs = self.pro.hs_const(hs_type=hs_type,is_new = is_new)
+        hs = self.col2(id_num = 104,data = hs)
+        return hs
+    """""
+    接口：stock_company
+    描述：获取上市公司基础信息
+    积分：用户需要至少120积分才可以调取，具体请参阅积分获取办法
+    
+    输入参数
+    
+    名称	类型	默认显示	描述
+    exchange	str	N	交易所代码 ，SSE上交所 SZSE深交所 ，默认SSE
+    输出参数
+    """""
+    def stoc_company(self,exchange = '',fields = ''):
+        if len(fileds) ==0 :
+            sc = self.pro.stock_company(exchange= exchagne)
+        else:
+            sc = self.pro.stock_compnay(exchange = exchange , fields = fields)
+        sc = self.col2(id_num = 112,data =sc)
+        return sc
+    """""
+    IPO新股列表
+    接口：new_share
+    描述：获取新股上市列表数据
+    限量：单次最大2000条，总量不限制
+    积分：用户需要至少120积分才可以调取，具体请参阅积分获取办法
+    
+    输入参数
+    
+    名称	类型	必选	描述
+    start_date	str	N	上网发行开始日期
+    end_date	str	N	上网发行结束日期
+    """""
+    def IPO(self,start_date = self.start_date,end_date = self.end_date):
+        ipo = self.pro.new_share(start_date=start_date, end_date=end_date)
+        ipo = self.col2(id_num = 123 ,data =ipo)
+        return ipo
 T = Tushare_data()
-print(T.trade_cal())
 
+print(T.hs_const())
